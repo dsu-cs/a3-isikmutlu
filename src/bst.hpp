3529db95bc89e8e6525294fa5bec154bc4535659
isikmutlu <54554070+isikmutlu@users.noreply.github.com>
2019-11-18 16:16:04 +0300

    Update bst.hpp

--- src/bst.hpp
+++ src/bst.hpp
@@ -3,6 +3,7 @@
 // include this library to use NULL, otherwise use nullptr instead
 #include <cstddef>
 #include <iostream>
+#include <vector>
 #include "node.hpp"
 
 template<class T>
@@ -15,6 +16,7 @@ public:
     // Inserts data into the tree
     // param: The data to be inserted into the tree
     void insert(T);
+
     // Removes data from the tree
     // param: The data to be removed from the tree
     void remove(T);
@@ -26,6 +28,7 @@ public:
     std::vector<T> *postorder(void);
     // Performs an preorder traversal
     // returns: pointer to a vector containing the tree traversal
+
     std::vector<T> *preorder(void);
     // Searches the tree for a given value
     // param: the data to search for
@@ -35,11 +38,26 @@ public:
     // Gets the current number of nodes in the tree
     // returns: the number of nodes in the tree
     int get_size(void);
+
 private:
     // the root node of the tree
     Node<T> *root;
     // the number of nodes in the tree
     int node_count;
+    // insert helper function
+    void insertHelper(Node<T>*, T);
+    //search helper function
+    void searchHelper(Node<T>*, T);
+    //remove helper fuction
+    void removeHelper(Node<T>*, T);
+    //get size helper function
+    void get_size(Node<T>*, T);
+    //inorder helper
+    void inorderHelper(Node<T>*);
+    //preorder helper
+    void preorderHelper(Node<T>*);
+    //post order helper
+    void postorderHelper(Node<T>*);
 };
 
 template<class T>
@@ -60,55 +78,160 @@ BST<T>::~BST()
 }
 
 template<class T>
- std::vector<T> * BST<T>::inorder()
-{
-    std::vector<T> *vec = new std::vector<T>;
 
-    return vec;
-}
+ std::vector<T> *BST<T>::inorder() {
+std::vector<T> *vec = new std::vector<T>;
+        inorderHelper(root);
 
-
-template<class T>
- std::vector<T> * BST<T>::preorder()
-{
-    std::vector<T> *vec = new std::vector<T>;
-    return vec;
-}
-
-
-template<class T>
- std::vector<T> * BST<T>::postorder()
-{
-    std::vector<T> *vec = new std::vector<T>;
-
-    return vec;
 }
 
 template<class T>
-void BST<T>::insert(T new_data)
-{
 
+void BST<T>::inorderHelper(Node<T>* node){
+    std::vector<T> *vec = new std::vector<T>;
+
+
+        if(node != NULL) {
+        if(node->left) inorderHelper(node->left);
+        std::cout << " " << node->val << " ";
+        if(node->right)
+        inorderHelper(node->right);
+    }
 }
 
+
+
+/*pseudocode for preorder traversal
+         print the node
+         recurse the left subtree
+         recurse the right subtree
+    */
+template<class T>
+ std::vector<T>* BST<T>::preorder(){
+    std::vector<T> *vec = new std::vector<T>;
+
+    preorderHelper(root);
+
+ }
+template<class T>
+    void BST<T>::preorderHelper(Node<T>* node){
+        std::vector<T> *vec = new std::vector<T>;
+        if(node != NULL) {
+        std::cout << " " << node->val << " ";
+        if(node->left) prederHelper(node->left);
+        if(node->right) preorderHelper(node->right);
+    }
+    }
+
+
+template<class T>
+    std::vector<T>* BST<T>::postorder(){
+    std::vector<T> *vec = new std::vector<T>;
+    postorderHelper(root);
+ }
+
+template<class T>
+    void BST<T>::postorderHelper(Node<T>* node){
+    std::vector<T> *vec = new std::vector<T>;
+    if(node != NULL) {
+        if(node->left) postorderHelper(node->left);
+        if(node->right) postorderHelper(node->right);
+        std::cout << " " << node->val << " ";
+    }
+    }
+
+template<class T>
+void BST<T>::insert(T val)
+{
+        insertHelper(root, val);
+}
+template<class T>
+void BST<T>::insertHelper(Node<T>* root, T val) {
+        if (root == NULL) { root= Node<T>* (val);}
+        if (val < root->new_data) root->left = insertHelper(root->left, val);
+        if (val > root->new_data) root->right = insertHelper(root->right, val);
+        return root;
+    }
 
 template<class T>
 Node<T> *BST<T>::search(T val)
 {
+    return search(Node* root, T val);
+}
+
+template<class T>
+Node<T> *BST<T>::search(Node* root, T val)
+{
+    if (root == nullptr) return -1;
+    if (root->val == val) return root;
+    return val > root->val ? search(root->right, val) : search(root->left, val);
 
 }
 
 
 
 template<class T>
-void BST<T>::remove(T val)
+void BST<T>::remove(Node *root, T val)
+{
+    root=remove(root, val);
+}
+Node* BST<T>::remove(Node *root, T val)
 {
 
+if (root == NULL) {
+     return NULL;
+  }
+  if (val < root->val) {  // data is in the left sub tree.
+      root->left = Delete(root->left, val);
+  } else if (data > root->val) { // val is in the right sub tree.
+      root->right = Delete(root->right, val);
+  } else {
+     // case 1: no children
+     if (root->left == NULL && root->right == NULL) {
+        delete(root); // wipe out the memory, in C, use free function
+        root = NULL;
+     }
+     // case 2: one child (right)
+     else if (root->left == NULL) {
+        struct Node *temp = root; // save current node as a backup
+        root = root->right;
+        delete temp;
+     }
+     // case 3: one child (left)
+     else if (root->right == NULL) {
+        struct Node *temp = root; // save current node as a backup
+        root = root->left;
+        delete temp;
+     }
+     // case 4: two children
+     else {
+        struct Node *temp = FindMin(root->right); // find minimal value of right sub tree
+        root->val = temp->val; // duplicate the node
+        root->right = Delete(root->right, temp->val); // delete the duplicate node
+     }
+  }
+  return root; // parent node can update reference
 }
 
 
 
+
+
+template<class T>
+Node* BST<T>::int get_size(Node*root)//helper function for get_size
+{
+	if (node == NULL)
+        return 0;
+    else
+        return(size(node->left) + 1 + size(node->right));  //+1 is for root
+}
 template<class T>
 int BST<T>::get_size()
 {
-
+    n=0;
+    if (root != NULL) {
+    n = get_size(root);
+    }
+    return n;
 }
+
